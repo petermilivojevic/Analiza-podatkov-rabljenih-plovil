81cad9e552d36fc427bb5e268a3c66dd854d65fa
petermilivojevic <peter.milivojevic@gmail.com>
2023-07-26 21:02:59 +0200

    Koda pridobi html-je in ustvari ne urejen csv

--- Pridobivanje_podatkov.py
+++ Pridobivanje_podatkov.py
@@ -1,7 +1,6 @@
 import re
-
-with open("Poizkusni.html") as dat:
-    vsebina = dat.read()
+import requests
+import csv
 
 vzorec = re.compile(
     r'<p class="blurb__title-header">(?P<kategorija>[^<]*)</p>'
@@ -15,6 +14,53 @@ vzorec = re.compile(
     r'.*<ul class="blurb__description"><li>(?P<uporabljenost>[^<]*)'
 )
 
-for i, ujemanje in enumerate(vzorec.finditer(vsebina), 1):
-    print(i, ujemanje.groupdict())
+def izloci_podatke_bark(blok):
+    barka = vzorec.search(blok).groupdict()
+    barka['ime'] = barka['ime']
+    barka['kategorija']  = barka['kategorija'].strip().split(', ')
+    barka['dimenzija'] = barka['dimenzija']
+    barka['leto'] = int(barka['leto'])
+    barka['uporabljenost'] = barka['uporabljenost']
+    barka['cena'] = barka['cena'].replace('.', '')
+    barka['lokacija'] = barka['lokacija']
+    return barka
 
+
+
+
+
+def ime_datoteke(st_strani):
+    return f"Rabljena-plovila-{st_strani + 1}.html"
+
+for i in range(58):
+    if i == 1:
+        url = (
+        'https://www.boat24.com/en/secondhandboats/?bre_max=999&bre_min=1&lge_max=999&lge_min=1&rgo%5B0%5D=51&rgo%5B1%5D=31'
+    )
+    else:
+        url = (
+            'https://www.boat24.com/en/secondhandboats/?bre_max=999&bre_min=1&lge_max=999&lge_min=1&'
+            f'page={20 * i}&rgo%5B0%5D=51&rgo%5B1%5D=31'
+    )
+    response = requests.get(url)
+    with open(ime_datoteke(i), 'w') as dat:
+        dat.write(response.text) 
+
+barke = []
+
+for i in range(58):
+    with open(ime_datoteke(i)) as dat:
+        vsebina = dat.read()
+    for blok in vzorec.finditer(vsebina):
+        barke.append(izloci_podatke_bark(blok.group(0)))
+
+with open('barke.csv', 'w', newline='') as dat:
+    pisatelj = csv.writer(dat)
+    for barka in barke:
+        ime, kategorija, dimenzija, leto = barka['ime'], barka['kategorija'], barka['dimenzija'], barka['leto']
+        uporabljenost, cena, lokacija = barka['uporabljenost'], barka['cena'], barka['lokacija']
+        pisatelj.writerow([ime, kategorija, dimenzija, leto, uporabljenost, cena, lokacija])
+
+
+#for i, ujemanje in enumerate(vzorec.finditer(vsebina), 1):
+#    print(i, ujemanje.groupdict())
