28bdf10b27b2b29da73788789d61fb3a555f195c
petermilivojevic <peter.milivojevic@gmail.com>
2023-08-17 13:36:07 +0200

    Nekoliko bolj obsežna analiza, ki podrobneje preverja hipotezo, da dobimo boljšo ponudbo za nakup bark na Hrvaškem in doda graf, ki prikazuje, da je večina bark v razmerju 1:5 (sirina:dolzina)

--- Poberi_podatke.py
+++ Poberi_podatke.py
@@ -7,6 +7,9 @@ imena_polj = [
     'id', 'ime', 'kategorija', 'dolzina', 'sirina',
     'leto', 'uporabljenost', 'cena', 'lokacija'
     ]
+
+# Ta csv se ne bo uporabljal v nadalnji analizi, vendar ga bom vseeno obdržal,
+# saj mi je bil v veliko pomoč pri preverjanju, če koda deluje pravilno.
 zapisi_csv(barke, imena_polj, 'barke.csv')
 
 kategorije = izloci_gnezdene_podatke(barke)
